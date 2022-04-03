3a1966f25f373fa92781bde4e8db89b402aa9e2a
Chutiiing <58101976+Chutiiing@users.noreply.github.com>
2022-04-04 00:22:48 +0800

    init

---
+++ src/main/java/com/chutiiing/backendsystem/BackEndSystemApplication.java
@@ -0,0 +1,20 @@
+package com.chutiiing.backendsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class BackEndSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndSystemApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "ok";
+    }
+}
---
+++ src/test/java/com/chutiiing/backendsystem/BackEndSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chutiiing.backendsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
